1fa29234baac4e1626dd7e105d9946f4c74e2896
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2016-05-12 17:00:01 -0700

    Added user update

--- cedar-user-operations/src/main/java/org/metadatacenter/server/dao/GenericUserDao.java
+++ cedar-user-operations/src/main/java/org/metadatacenter/server/dao/GenericUserDao.java
@@ -1,8 +1,10 @@
 package org.metadatacenter.server.dao;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.metadatacenter.server.security.model.user.CedarUser;
 
+import javax.management.InstanceNotFoundException;
 import java.io.IOException;
 
 public interface GenericUserDao {
@@ -10,4 +12,6 @@ public interface GenericUserDao {
   @NonNull CedarUser create(@NonNull CedarUser element) throws IOException;
 
   CedarUser find(@NonNull String id) throws IOException;
+
+  @NonNull CedarUser update(@NonNull String userId, JsonNode modification) throws IOException, InstanceNotFoundException;
 }
