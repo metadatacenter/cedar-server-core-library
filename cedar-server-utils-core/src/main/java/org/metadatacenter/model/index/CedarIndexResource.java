988455c18805b7eff8bd56b7f0330cef23e1a2ef
Marcos Martinez <marcos.martinez.romero@gmail.com>
2016-09-09 17:57:28 -0700

    Add templateId to CedarIndexResource

--- cedar-server-utils-core/src/main/java/org/metadatacenter/model/index/CedarIndexResource.java
+++ cedar-server-utils-core/src/main/java/org/metadatacenter/model/index/CedarIndexResource.java
@@ -9,14 +9,17 @@ public class CedarIndexResource {
   private CedarRSNode info;
   private List<String> fieldName;
   private List<String> fieldValue;
+  // Only for template instances
+  private String templateId;
 
   // Used by Jackson
   public CedarIndexResource() {};
 
-  public CedarIndexResource(CedarRSNode info, List<String> fieldName, List<String> fieldValue) {
+  public CedarIndexResource(CedarRSNode info, List<String> fieldName, List<String> fieldValue, String templateId) {
     this.info = info;
     this.fieldName = fieldName;
     this.fieldValue = fieldValue;
+    this.templateId = templateId;
   }
 
   public CedarRSNode getInfo() {
@@ -42,5 +45,14 @@ public class CedarIndexResource {
   public void setFieldValue(List<String> fieldValue) {
     this.fieldValue = fieldValue;
   }
+
+  public String getTemplateId() {
+    return templateId;
+  }
+
+  public void setTemplateId(String templateId) {
+    this.templateId = templateId;
+  }
+
 }
 
