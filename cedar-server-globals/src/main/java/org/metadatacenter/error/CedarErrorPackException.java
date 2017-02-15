710168acd39a58ef77930aa5d3aac401637f4f22
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2017-02-15 00:00:04 -0800

    Exception cause serialized

--- cedar-server-globals/src/main/java/org/metadatacenter/error/CedarErrorPackException.java
+++ cedar-server-globals/src/main/java/org/metadatacenter/error/CedarErrorPackException.java
@@ -1,39 +1,34 @@
 package org.metadatacenter.error;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
-public class CedarErrorPackException {
+public class CedarErrorPackException extends CedarErrorPackSingleException {
 
-  private StackTraceElement[] stackTrace;
-  private String message;
-  private String localizedMessage;
+  private List<CedarErrorPackSingleException> causes;
 
   public CedarErrorPackException(Exception e) {
+    causes = new ArrayList<>();
     if (e != null) {
       this.message = e.getMessage();
       this.localizedMessage = e.getLocalizedMessage();
       this.stackTrace = e.getStackTrace();
-      Throwable cause = e.getCause();
-      if (cause != null) {
-        this.message += "\n" + cause.getMessage();
-        this.localizedMessage += "\n" + cause.getLocalizedMessage();
-        StackTraceElement[] causeStackTrace = cause.getStackTrace();
-        StackTraceElement[] result = Arrays.copyOf(stackTrace, stackTrace.length + causeStackTrace.length);
-        System.arraycopy(causeStackTrace, 0, result, stackTrace.length, causeStackTrace.length);
-        stackTrace = result;
-      }
+      appendCause(e.getCause());
     }
   }
 
-  public StackTraceElement[] getStackTrace() {
-    return stackTrace;
+  private void appendCause(Throwable cause) {
+    if (cause != null) {
+      CedarErrorPackSingleException c = new CedarErrorPackSingleException();
+      c.message = cause.getMessage();
+      c.localizedMessage = cause.getLocalizedMessage();
+      c.stackTrace = cause.getStackTrace();
+      causes.add(c);
+      appendCause(cause.getCause());
+    }
   }
 
-  public String getMessage() {
-    return message;
-  }
-
-  public String getLocalizedMessage() {
-    return localizedMessage;
+  public List<CedarErrorPackSingleException> getCauses() {
+    return causes;
   }
 }
--- cedar-server-globals/src/main/java/org/metadatacenter/exception/security/AuthorizationTypeNotFoundException.java
+++ cedar-server-globals/src/main/java/org/metadatacenter/exception/security/AuthorizationTypeNotFoundException.java
@@ -1,10 +1,14 @@
 package org.metadatacenter.exception.security;
 
 import org.metadatacenter.error.CedarErrorKey;
+import org.metadatacenter.error.CedarSuggestedAction;
+
+import javax.ws.rs.core.Response;
 
 public class AuthorizationTypeNotFoundException extends CedarAccessException {
 
   public AuthorizationTypeNotFoundException() {
-    super("Authorization type not found.", CedarErrorKey.AUTHORIZATION_TYPE_NOT_FOUND, null);
+    super("Authorization type not found.", CedarErrorKey.AUTHORIZATION_TYPE_NOT_FOUND, CedarSuggestedAction.LOGOUT);
+    errorPack.setStatus(Response.Status.FORBIDDEN);
   }
 }
