cf703693b80478e2d3009a61f2ed5e5550ca8685
Marcos Martinez <marcos.martinez.romero@gmail.com>
2017-02-27 11:40:16 -0800

    Add URL util methods

--- cedar-server-rest/src/main/java/org/metadatacenter/util/http/CedarUrlUtil.java
+++ cedar-server-rest/src/main/java/org/metadatacenter/util/http/CedarUrlUtil.java
@@ -6,7 +6,9 @@ import org.apache.http.HttpResponse;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
+import java.net.URLDecoder;
 import java.net.URLEncoder;
 
 public abstract class CedarUrlUtil {
@@ -18,6 +20,27 @@ public abstract class CedarUrlUtil {
       // Do nothing
     }
     return null;
+  }
+
+  public static String urlDecode(String value) {
+    try {
+      return URLDecoder.decode(value, "UTF-8");
+    } catch (Exception e) {
+      // Do nothing
+    }
+    return null;
+  }
+
+  public static String encodeIfNeeded(String uri) throws UnsupportedEncodingException {
+    String decodedUri = urlDecode(uri);
+    // It is necessary to encode it
+    if (uri.compareTo(decodedUri) == 0) {
+      return urlEncode(uri);
+    }
+    // If was already encoded
+    else {
+      return uri;
+    }
   }
 
   public static URI getIdURI(UriInfo uriInfo, String id) {
@@ -35,4 +58,6 @@ public abstract class CedarUrlUtil {
     }
     return location;
   }
+
+
 }
