b2f6cb69fee376133ffc17c6bff73e6807b289b6
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2017-03-02 17:25:39 -0800

    Mongo connection with authentication

---
+++ cedar-server-globals/src/main/java/org/metadatacenter/util/mongo/MongoClientFactory.java
@@ -0,0 +1,37 @@
+package org.metadatacenter.util.mongo;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import org.metadatacenter.config.MongoConnection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MongoClientFactory {
+
+  private MongoConnection mongoConnection;
+
+  private MongoClient mongoClient;
+
+  public MongoClientFactory(MongoConnection mongoConnection) {
+    this.mongoConnection = mongoConnection;
+  }
+
+  public void buildClient() {
+    ServerAddress address = new ServerAddress(mongoConnection.getHost(), mongoConnection.getPort());
+    List<MongoCredential> credentials = new ArrayList<>();
+    credentials.add(
+        MongoCredential.createScramSha1Credential(
+            mongoConnection.getUser(),
+            mongoConnection.getDatabaseName(),
+            mongoConnection.getPassword().toCharArray()
+        )
+    );
+    this.mongoClient = new MongoClient(address, credentials);
+  }
+
+  public MongoClient getClient() {
+    return mongoClient;
+  }
+}
