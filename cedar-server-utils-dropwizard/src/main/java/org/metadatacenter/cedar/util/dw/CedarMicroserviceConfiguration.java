8fc4775f67a33576fcc45823751cbb8e028f52f9
johardi <josef.hardi@stanford.edu>
2017-04-07 09:39:58 -0700

    Add a new config entry 'testPort' to allocate different port number for testing

--- cedar-server-utils-dropwizard/src/main/java/org/metadatacenter/cedar/util/dw/CedarMicroserviceConfiguration.java
+++ cedar-server-utils-dropwizard/src/main/java/org/metadatacenter/cedar/util/dw/CedarMicroserviceConfiguration.java
@@ -1,6 +1,16 @@
 package org.metadatacenter.cedar.util.dw;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
 
+import java.util.Optional;
+
 public class CedarMicroserviceConfiguration extends Configuration {
+
+  private Integer testPort;
+
+  @JsonProperty("testPort")
+  public Optional<Integer> getTestPort() {
+    return Optional.ofNullable(this.testPort);
+  }
 }
