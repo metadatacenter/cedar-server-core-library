1d664f68e337ce7006bd15e382afb8b897d96c13
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2017-07-14 13:26:42 -0700

    Fixed the test of environment

--- cedar-project-config/src/test/java/org/metadatacenter/config/CedarConfigTest.java
+++ cedar-project-config/src/test/java/org/metadatacenter/config/CedarConfigTest.java
@@ -49,6 +49,12 @@ public class CedarConfigTest {
     env.put(CedarEnvironmentVariable.CEDAR_MONGO_HOST.getName(), "localhost");
     env.put(CedarEnvironmentVariable.CEDAR_MONGO_PORT.getName(), "27017");
 
+    env.put(CedarEnvironmentVariable.CEDAR_MESSAGING_MYSQL_HOST.getName(), "127.0.0.1");
+    env.put(CedarEnvironmentVariable.CEDAR_MESSAGING_MYSQL_PORT.getName(), "3306");
+    env.put(CedarEnvironmentVariable.CEDAR_MESSAGING_MYSQL_DB.getName(), "cedar_messaging");
+    env.put(CedarEnvironmentVariable.CEDAR_MESSAGING_MYSQL_USER.getName(), "cedar_messaging_user");
+    env.put(CedarEnvironmentVariable.CEDAR_MESSAGING_MYSQL_PASSWORD.getName(), "cedar_messaging_password");
+
     env.put(CedarEnvironmentVariable.CEDAR_SALT_API_KEY.getName(), "salt");
 
     env.put(CedarEnvironmentVariable.CEDAR_BIOPORTAL_REST_BASE.getName(), "http://data.bioontology.org/");
