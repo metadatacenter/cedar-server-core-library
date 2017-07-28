658279fb925e118c13972e2e74c49d7db8a00615
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2017-07-28 10:11:50 -0700

    Added helper for messaging url

--- cedar-server-globals/src/main/java/org/metadatacenter/server/url/MicroserviceUrlUtil.java
+++ cedar-server-globals/src/main/java/org/metadatacenter/server/url/MicroserviceUrlUtil.java
@@ -7,11 +7,13 @@ public class MicroserviceUrlUtil {
   private final WorkspaceMicroserviceUrlProvider workspace;
   private final UserMicroserviceUrlProvider user;
   private final TemplateMicroserviceUrlProvider template;
+  private final MessagingMicroserviceUrlProvider messaging;
 
   public MicroserviceUrlUtil(ServersConfig servers) {
     workspace = new WorkspaceMicroserviceUrlProvider(servers.getWorkspace());
     user = new UserMicroserviceUrlProvider(servers.getUser());
     template = new TemplateMicroserviceUrlProvider(servers.getTemplate());
+    messaging = new MessagingMicroserviceUrlProvider(servers.getMessaging());
   }
 
   public WorkspaceMicroserviceUrlProvider getWorkspace() {
@@ -24,5 +26,9 @@ public class MicroserviceUrlUtil {
 
   public TemplateMicroserviceUrlProvider getTemplate() {
     return template;
+  }
+
+  public MessagingMicroserviceUrlProvider getMessaging() {
+    return messaging;
   }
 }
