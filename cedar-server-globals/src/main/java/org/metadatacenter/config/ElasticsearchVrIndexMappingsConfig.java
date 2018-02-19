f4c5a2e21e48950143f072245f1b8865f79aa4f2
Marcos Martinez <marcos.martinez.romero@gmail.com>
2018-02-19 12:57:14 -0800

    Update mappings for value recommender index

--- cedar-server-globals/src/main/java/org/metadatacenter/config/ElasticsearchVrIndexMappingsConfig.java
+++ cedar-server-globals/src/main/java/org/metadatacenter/config/ElasticsearchVrIndexMappingsConfig.java
@@ -1,5 +1,13 @@
 package org.metadatacenter.config;
 
+import java.util.HashMap;
+
 public class ElasticsearchVrIndexMappingsConfig {
+
+  private HashMap<String, Object> doc;
+
+  public HashMap<String, Object> getDoc() {
+    return doc;
+  }
 
 }
