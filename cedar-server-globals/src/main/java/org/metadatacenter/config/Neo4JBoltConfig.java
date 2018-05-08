86593da2c75b20ca3c731593e9c66089be530b9a
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2018-05-08 14:34:27 -0700

    Bolt config

---
+++ cedar-server-globals/src/main/java/org/metadatacenter/config/Neo4JBoltConfig.java
@@ -0,0 +1,22 @@
+package org.metadatacenter.config;
+
+public class Neo4JBoltConfig {
+
+  private String uri;
+
+  private String userName;
+
+  private String userPassword;
+
+  public String getUri() {
+    return uri;
+  }
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public String getUserPassword() {
+    return userPassword;
+  }
+}
