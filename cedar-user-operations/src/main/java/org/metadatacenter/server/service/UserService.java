00bcff15e930ea6a7499247c363c8186d43ffc72
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2019-04-18 11:27:26 -0700

    Refactor

--- cedar-user-operations/src/main/java/org/metadatacenter/server/service/UserService.java
+++ cedar-user-operations/src/main/java/org/metadatacenter/server/service/UserService.java
@@ -5,21 +5,20 @@ import org.metadatacenter.server.result.BackendCallResult;
 import org.metadatacenter.server.security.IUserService;
 import org.metadatacenter.server.security.model.user.CedarUser;
 
-import java.io.IOException;
 import java.util.List;
 
 public interface UserService extends IUserService {
 
-  CedarUser createUser(CedarUser user) throws IOException;
+  CedarUser createUser(CedarUser user);
 
-  CedarUser findUser(String userId) throws IOException;
+  CedarUser findUser(String userId);
 
-  CedarUser findUserByApiKey(String apiKey) throws IOException;
+  CedarUser findUserByApiKey(String apiKey);
 
   BackendCallResult<CedarUser> updateUser(CedarUser user);
 
   BackendCallResult<CedarUser> patchUser(String userId, JsonNode modifications);
 
-  List<CedarUser> findAll() throws IOException;
+  List<CedarUser> findAll();
 
 }
